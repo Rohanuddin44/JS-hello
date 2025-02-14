9a63039ee931f07148bdf72b720d84591e178e59
Rohanuddin44 <uddin657321@gmail.com>
2025-02-14 16:44:32 +0500

    files added

---
+++ app.js
@@ -0,0 +1,41 @@
+// Chapter-1 (Alert)
+// Q-1
+alert('first name')
+alert('Last name')
+alert('Email')
+alert('Phone number')
+alert('password')
+
+// Q-2
+alert('You`re Learning JavaScript')
+
+// Q-3
+alert('Hello JavaScript')
+// ----------
+
+// Chapter-2 (Variables for string)
+// Q-1
+variableName = 'Hello JavaScript';
+
+// Q-2
+variableName = '';
+variableName = 'Hello JavaScript';
+
+// Q-3
+teamName = 'Champion danger';
+alert('Champion danger')
+
+// Q-4
+bestMan = 'Charlie';
+bestMan = 'John';  
+
+
+// Chapter 3 (Variables for numbers)
+// Q-1
+caseQty = '';
+
+// Q-2
+caseQty = '144';
+
+
+
